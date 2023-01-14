0aee336a935a7bd330f2e6d82dc79e4051daf800
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-01-14 11:19:29 +0500

    change model, templates, add vacancies in db(550 Mb now ~_~)

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -114,7 +114,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    # '/var/www/static/',
 ]
 
 # Default primary key field type
--- myapp1/views.py
+++ myapp1/views.py
@@ -1,37 +1,70 @@
-from django.db.models import QuerySet
-from django.shortcuts import render
+# from django.db.models import QuerySet
+from django.shortcuts import render, redirect
 from django.core.handlers.wsgi import WSGIRequest
-from rest_framework.viewsets import ModelViewSet
+from django.db.models import Q
+# from rest_framework.viewsets import ModelViewSet
 
+from myapp1.forms import VacancyForm
 from myapp1.models import Vacancy
-from myapp1.serializers import VacancySerializer
+
+
+# from myapp1.serializers import VacancySerializer
 
 
 # Create your views here.
 
 def index_page(request: WSGIRequest) -> render:
-    vacancies = Vacancy.objects.all()
-    # for vac in vacancies:
-    #     print(f"ID = {vac.id}, Фамилия = {vac.second_name}, Имя = {vac.name}, Запрлата = {vac.salary}")
+    vacancies = Vacancy.objects.filter(Q(name__icontains='python-программист') | Q(name__icontains='пайтон')
+                                       | Q(name__icontains='python') | Q(name__icontains='пайтон'))
 
-    return render(request, 'main.html', {"vacancies": vacancies})
+    return render(request, 'main.html', {'vacancies': vacancies})
+
 
 def relevance_page(request: WSGIRequest) -> render:
-    return render(request, 'relevance.html')
+    profession_name = "Python-разработчик"
+    header_year = ["Год", "Средняя зарплата", f"Средняя зарплата - {profession_name}", "Количество вакансий",
+                   f"Количество вакансий - {profession_name}"]
+    # context = {'salary_data': zip(salary_data,
+    context = {
+        'header_year': header_year,
+        'profession_name': f"{profession_name}"}
+
+    return render(request, 'relevance.html', context)
+
 
 def geography_page(request: WSGIRequest) -> render:
-    return render(request, 'geography.html')
+    return render(request, 'geography.html')  # TODO: Написать логику обработки данных
+
 
 def skills_page(request: WSGIRequest) -> render:
     return render(request, 'skills.html')
 
+
 def recent_vacancies_page(request: WSGIRequest) -> render:
-    return render(request, 'recent vacancies.html')
+    return render(request, 'recent_vacancies.html')
+
+
+def add_vacancy(request: WSGIRequest) -> render:
+    error_message = ""
+    if request.method == "POST":
+        received_form = VacancyForm(request.POST)
+        if received_form.is_valid():
+            received_form.save()
+            return redirect('home')
+        else:
+            error_message = "Неправильно заполненная форма."
+
+    form = VacancyForm()
+    data = {
+        'form': form,
+        'error_message': error_message
+    }
+    return render(request, 'add_vacancy.html', data)
+
 
 def vue_page(request: WSGIRequest) -> render:
     return render(request, "vue.html")
 
-
-class ExampleView(ModelViewSet):
-    queryset: QuerySet = Vacancy.objects.all()
-    serializer_class = VacancySerializer
+# class ExampleView(ModelViewSet):
+#     queryset: QuerySet = Vacancy.objects.all()
+#     serializer_class = VacancySerializer
