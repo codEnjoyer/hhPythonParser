14c1187b58efb13336bd4d104708d0e925e54fc6
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-01-15 20:51:03 +0500

    add recent_vacancies

--- myapp1/views.py
+++ myapp1/views.py
@@ -1,14 +1,22 @@
 # from django.db.models import QuerySet
 from django.shortcuts import render, redirect
 from django.core.handlers.wsgi import WSGIRequest
-from django.db.models import Q, Avg, Count, F
+from django.db.models import Q, Avg, Count
 # from rest_framework.viewsets import ModelViewSet
 
 from myapp1.forms import VacancyForm
 from myapp1.models import Vacancy
 
-
+from collections import Counter
 # from myapp1.serializers import VacancySerializer
+
+import time
+import requests
+import json
+import pandas as pd
+from math import ceil
+import datetime
+from bs4 import BeautifulSoup
 
 
 # Create your views here.
@@ -29,7 +37,7 @@ def relevance_page(request: WSGIRequest) -> render:
                                .annotate(total_count=Count('id'), avg_salary=Avg('salary'),
                                          prof_count=prof_count, prof_salary=prof_salary)
                                .values('published_at', 'total_count', 'avg_salary', 'prof_count', 'prof_salary')
-                               .order_by())
+                               .order_by('-published_at'))
     data = {
         'header_year': header_year,
         'profession_name': f"{profession_name}",
@@ -41,7 +49,7 @@ def relevance_page(request: WSGIRequest) -> render:
 
 def geography_page(request: WSGIRequest) -> render:
     profession_name = "Python-разработчик"
-    header = ["Город", "Всего вакансий", "Средняя зарплата", "Доля вакансий"]
+    header = ["Город", "Всего вакансий", "Средняя зарплата", f"Вакансий {profession_name}"]
 
     prof_filter = Q(name__icontains=f'{profession_name}') | Q(name__icontains='python') | Q(name__icontains='питон')
     prof_count = Count('id', filter=prof_filter)
@@ -51,7 +59,7 @@ def geography_page(request: WSGIRequest) -> render:
                                           avg_salary=Avg('salary'),
                                           prof_count=prof_count)
                                 .values('area_name', 'total_count', 'avg_salary', 'prof_count')
-                                .order_by('-prof_count')[:10]) # TODO: Перевод в проценты
+                                .order_by('-prof_count')[:10])
     data = {
         "profession_name": profession_name,
         "header_city": header,
@@ -61,11 +69,35 @@ def geography_page(request: WSGIRequest) -> render:
 
 
 def skills_page(request: WSGIRequest) -> render:
-    return render(request, 'skills.html')
+    all_skills = Vacancy.objects.exclude(key_skills=None).values('key_skills', 'published_at')
+    skills_by_year = {}
+    for c_skill in all_skills:
+        year = c_skill["published_at"]
+        if year not in skills_by_year.keys():
+            skills_by_year[year] = c_skill["key_skills"].split('\n')
+        else:
+            skills_by_year[year].extend(c_skill["key_skills"].split('\n'))
+
+    for year, skills in skills_by_year.items():
+        c = Counter(skills)
+        skills_by_year[year] = [(name, c[name] / len(skills) * 100.0) for name, count in c.most_common(10)]
+
+    data = {
+        "skills_by_year": dict(sorted(skills_by_year.items(), reverse=True))
+    }
+    return render(request, 'skills.html', data)
 
 
 def recent_vacancies_page(request: WSGIRequest) -> render:
-    return render(request, 'recent_vacancies.html')
+    header = ["Название", "Описание", "Навыки", "Компания", "Оклад", "Название региона", "Дата публикации"]
+    date_from = datetime.datetime(year=2022, month=12, day=15)
+    max_vacancies_count = 10
+    vacancies = Parser(date_from, max_vacancies_count).vacancies.to_dict(orient='records')
+    data = {
+        'vacancies': vacancies,
+        'headers': header
+    }
+    return render(request, 'recent_vacancies.html', data)
 
 
 def add_vacancy(request: WSGIRequest) -> render:
@@ -89,6 +121,158 @@ def add_vacancy(request: WSGIRequest) -> render:
 def vue_page(request: WSGIRequest) -> render:
     return render(request, "vue.html")
 
+
 # class ExampleView(ModelViewSet):
 #     queryset: QuerySet = Vacancy.objects.all()
 #     serializer_class = VacancySerializer
+
+# region HH_Parser
+def get_page(params: dict, id: str = '') -> dict:
+    """
+    Получает ответ от api.hh.ru/vacancies с заданными параметрами params.
+
+    :param params: Параметры для поиска вакансий.
+    :param id: ID вакансии.
+
+    :returns: Словарь, полученный в результате обработки полученного JSON-объекта.
+    """
+
+    req = requests.get('https://api.hh.ru/vacancies/' + id, params)
+    data = req.content.decode()
+    return json.loads(data)
+
+
+def get_relevant_vacancy_fields(vacancy_data: dict) -> list:
+    """
+    Возвращает только те поля вакансии, которые были указаны в ТЗ (name, salary_from, salary_to, salary_currency,
+    area_name, published_at).
+
+    :param vacancy_data: Словарь с данными об одной вакансии.
+
+    :returns: Список из полей вакансии вида [name, salary_from, salary_to, salary_currency, area_name, published_at].
+    """
+
+    more_info = get_page({}, vacancy_data['id'])
+
+    name = vacancy_data["name"]
+
+    raw_desc = more_info["description"]
+    soup = BeautifulSoup(raw_desc, 'html.parser')
+    description = soup.get_text()
+
+    key_skills = ", ".join([s for d in more_info["key_skills"] for s in d.values()])
+
+    company = vacancy_data["employer"]["name"]
+    area_name = vacancy_data["area"]["name"]
+    published_at = vacancy_data["published_at"]
+
+    salary = vacancy_data["salary"]
+    if salary is not None:
+        salary_from = salary["from"]
+        salary_to = salary["to"]
+        salary_currency = salary["currency"]
+    else:
+        salary_from, salary_to, salary_currency = None, None, None
+    return [name, description, key_skills, salary_from, salary_to, salary_currency, company, area_name, published_at]
+
+
+def get_fields_from_vacancies_page(vac_data: dict) -> pd.DataFrame:
+    """
+    Обрабатывает словарь страницы с вакансиями.
+
+    :param vac_data: Словарь страницы с вакансиями.
+
+    :returns: DataFrame со столбцами ["name", "salary_from", "salary_to", "salary_currency", "area_name",
+    "published_at"] из словаря с вакансиями.
+    """
+
+    parsed_relevant_vacancies_fields = [get_relevant_vacancy_fields(values) for values in vac_data["items"]]
+
+    t = pd.DataFrame(parsed_relevant_vacancies_fields,
+                     columns=["name", "description", "key_skills", "salary_from", "salary_to", "salary_currency",
+                              "company", "area_name",
+                              "published_at"])
+    return t
+
+
+def get_vacancies(date_from: datetime.datetime, date_to: datetime.datetime, max_vacancies_count: int,
+                  per_page: int = 100, pages: int = 20) -> pd.DataFrame:
+    """
+    Получает вакансии с hh.ru за последние n_hours часов. Для правильного подсчёта необходимо указать максимально
+    возможное количество получаемых вакансий. Per_page * pages не должно быть больше 2000.
+
+    :param date_from: Дата, с момента которой будут получены вакансии.
+    :param date_to: Дата, до которого момента будут получены вакансии.
+    :param max_vacancies_count: Максимально возможное количество получаемых вакансий. Из-за ограничения на выгрузку
+    более чем 2000 вакансий их получение организовано через некоторые периоды времени, величина которых зависит от этого
+     параметра.
+    :param per_page: Количество вакансий, получаемых на одной странице. Не может быть больше 100.
+    :param pages: Количество страниц, получаемых за один период времени.
+
+    :returns: DataFrame с полученными вакансиями за последние n_hours со столбцами ["name", "salary_from", "salary_to",
+    "salary_currency", "area_name", "published_at"].
+    """
+
+    df = pd.DataFrame(
+        columns=["name", "description", "key_skills", "salary_from", "salary_to", "salary_currency", "company",
+                 "area_name", "published_at"])
+
+    parsing_iterations = 30  # ceil(max_vacancies_count / (per_page * pages))
+    timedelta = date_to - date_from
+    n_hours = timedelta.days * 24 + timedelta.seconds / 3600
+    time_chunk = ceil(n_hours / parsing_iterations)
+    time_from = date_from.replace(second=0, microsecond=0)
+
+    for iteration in range(parsing_iterations):
+        time_to = time_from + datetime.timedelta(hours=time_chunk)
+
+        if time_to > date_to or df.shape[0] >= max_vacancies_count:
+            break
+
+        for page in range(pages):
+            params = {
+                'text': 'python-разработчик OR python-developer OR специалист-python OR python OR питон',
+                'per_page': per_page,
+                'page': page,
+                'specialization': 1,
+                'date_from': time_from.isoformat(),
+                'date_to': time_to.isoformat(),
+                'only_with_salary': True,
+                'currency': 'RUR'
+            }
+
+            vacancies_page = get_page(params)
+            if page == vacancies_page["pages"]:
+                break
+
+            t = get_fields_from_vacancies_page(vacancies_page)
+            df = pd.concat([df, t], ignore_index=True)
+
+            # print(df.shape)
+            time.sleep(0.1)
+
+        time_from = time_to
+    return df
+
+
+class Parser:
+    date_from: datetime.datetime
+    date_to: datetime.datetime
+    max_vacancies_count: int
+    vacancies: pd.DataFrame
+
+    def __init__(self, date_from: datetime.datetime, max_vacancies_count: int):
+        self.date_from = date_from
+        self.date_to = date_from + datetime.timedelta(days=1)
+        self.max_vacancies_count = max_vacancies_count
+        df = get_vacancies(self.date_from, self.date_to, self.max_vacancies_count)
+        self.vacancies = (df
+                          .assign(published_at=df["published_at"].astype('datetime64'),
+                                  salary_from=df[["salary_from", "salary_to"]]
+                                  .mean(axis=1).astype('int32'))
+                          .rename({"salary_from": "salary"}, axis="columns")
+                          .drop(columns=["salary_currency", "salary_to"])
+                          .sort_values(by=['published_at'], ascending=True)
+                          )[:max_vacancies_count]
+
+# endregion
